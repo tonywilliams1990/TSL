004610cce002cbde79d548c40a2aaced8b7f4807
tonywilliams1990 <tony.williams.1990@gmail.com>
2016-10-10 20:05:22 +0100

    AutoDiff Jacobian

--- Examples/Test/AutoDiff_test.cpp
+++ Examples/Test/AutoDiff_test.cpp
@@ -18,9 +18,45 @@ F test_function_2( const F& x )
 template <class F>
 F test_grad_func( const TSL::Vector<F>& x )
 {
+  if ( x.size() != 3 ){ throw TSL::Error( "test_grad_func input size error" );}
   return x[0] * x[1] * x[2];
 }
 
+template <class F>
+TSL::Vector<F> test_jac_func( const TSL::Vector<F>& x )
+{
+  if ( x.size() != 2 ){ throw TSL::Error( "test_jac_func input size error" );}
+  TSL::Vector<F> temp( 3 );
+  temp[ 0 ] = x[ 0 ] * x[ 0 ] * x[ 1 ];
+  temp[ 1 ] = 5.0 * x[ 0 ] + 2.0 * x[ 1 ];
+  temp[ 2 ] = 3.0 * x[ 0 ];
+  // f(x,y) = [ x^2 * y, 5*x + 2*y, 3*x ]^T
+  return temp;
+}
+
+namespace TSL
+{
+  template <class T>
+
+  class test_residual : public Residual<T>
+  {
+    public:
+			// The test equation is 2nd order
+			test_residual() : Residual<T> ( 2 ) {}
+
+      // Define the equation
+			void residual_fn( const Vector<T>& x_k, Vector<T>& f ) const
+			{
+				f[ 0 ] = TSL::pow( x_k[ 0 ], 3.0 ) + x_k[ 1 ] - 1;
+				f[ 1 ] = TSL::pow( x_k[ 1 ], 3.0 ) - x_k[ 0 ] + 1; 
+				/*
+					x^3 + y - 1 = 0,
+					y^3 - x + 1 = 0,
+					(x,y) = (1,0) is the only solution 
+				*/
+			}		
+  }; // End of class test_residual
+} // End of namespace TSL
 
 int main()
 {
@@ -57,6 +93,29 @@ int main()
   TSL::Vector<double> grad_f( 3, 0.0 );
   grad_f = diff.gradient( &test_grad_func, x_vec );
   cout << "grad_f = " << endl << grad_f << endl;
+
+  // Test the gradient function (vector of inputs)
+  TSL::Vector< TSL::Vector<double> > x_vecs( 3, x_vec );
+  TSL::Vector< TSL::Vector<double> > grads( 3 );
+  grads = diff.gradient(  &test_grad_func, x_vecs );
+
+  for ( std::size_t i=0; i<x_vecs.size(); ++i )
+  {
+    cout << "grad[" << i << "] = " << endl << grads[ i ] << endl;
+  }
+
+  // Test the Jacobian function (single input)
+  TSL::Vector<double> x_jac( 2 );
+  x_jac[ 0 ] = 2.0; x_jac[ 1 ] = 3.0;
+  cout << "x_jac = " << endl << x_jac << endl;
+  cout << "f( x_jac ) = " << endl << test_jac_func( x_jac ) << endl;
+  TSL::Matrix<double> jac( 3, 2, 0.0);
+  jac = diff.jacobian( &test_jac_func, x_jac );
+  cout << "J = " << endl << jac << endl;
+
+  // Test the Jacobian function (vector of inputs)
+  
+  
 
 	cout << "FINISHED" << endl;
 
--- include/AutoDiff.h
+++ include/AutoDiff.h
@@ -7,6 +7,8 @@
 #include <iostream>
 
 #include "Vector.h"
+#include "Matrix.h"
+#include "Residual.h"
 #include "Error.h"
 #include "Dual.h"
 
@@ -48,7 +50,7 @@ namespace TSL
           Dual<T> dual( x_vals[ 0 ], eps );
           Dual<T> result;
           Vector<T> ans( x_vals.size() );
-          for (std::size_t i=0; i<x_vals.size(); ++i)
+          for ( std::size_t i=0; i<x_vals.size(); ++i )
           {
             dual[ 0 ] = x_vals[ i ];
             result = func_ptr( dual );
@@ -60,12 +62,13 @@ namespace TSL
         /// Calculate the gradient of a scalar field at a point
         Vector<T> gradient( F (*func_ptr)(const Vector<F>&), const Vector<T>& x_val )
         {
-          Vector<T> eps( x_val.size(), 0.0 );
+          const std::size_t n( x_val.size() );
+          Vector<T> eps( n, 0.0 );
           Dual<T> result;
           Dual<T> dual( x_val[ 0 ], eps );
-          Vector< Dual<T> > dual_vec( x_val.size(), dual );
+          Vector< Dual<T> > dual_vec( n, dual );
           // Fill the vector of dual numbers
-          for ( std::size_t i=0; i < x_val.size(); ++i )
+          for ( std::size_t i=0; i < n; ++i )
           {
             dual_vec[ i ][ 0 ] = x_val[ i ];
             dual_vec[ i ].epsilons()[ i ] = 1.0; 
@@ -73,7 +76,52 @@ namespace TSL
           result = func_ptr( dual_vec );
           return result.epsilons();
         }
-   
+
+        /// Calculate the gradient of a scalar field at points stored in a vector
+        Vector< Vector<T> > gradient( F (*func_ptr)(const Vector<F>&), 
+                                      const Vector< Vector<T> >& x_vals )
+        {
+          Vector< Vector<T> > ans( x_vals.size() );
+          for ( std::size_t i=0; i<x_vals.size(); ++i )
+          {
+            ans[ i ] = gradient( func_ptr, x_vals[ i ] ); 
+          }
+          return ans;
+        }
+
+        /// Calculate the Jacobian of a vector valued function at a point
+        Matrix<T> jacobian( Vector<F> (*func_ptr)(const Vector<F>&), 
+                            const Vector<T>& x_val )
+        {
+          const std::size_t n( x_val.size() );
+          Vector<T> eps( n, 0.0 );
+          Dual<T> dual( x_val[ 0 ], eps );
+          Vector< Dual<T> > dual_vec( n, dual );
+          // Fill the vector of dual numbers
+          for (std::size_t i=0; i<n; ++i )
+          {
+            dual_vec[ i ][ 0 ] = x_val[ i ];
+            dual_vec[ i ].epsilons()[ i ] = 1.0;
+          }
+          Vector< Dual<T> > result;
+          result = func_ptr( dual_vec );
+          const std::size_t m( result.size() );
+          // Fill the matrix
+          Matrix<T> temp( m, n, x_val[0] );
+          for ( std::size_t i=0; i < m; ++i )
+          {
+            for ( std::size_t j=0; j< n; ++j )
+            {
+              temp( i, j ) = result[ i ].epsilons()[ j ];
+            }
+          }
+          return temp;
+        }
+        
+        ///TODO Calculate the Jacobian at points stored in a vector
+        
+
+        //TODO Use dual numbers in Residual class to calculate the Jacobian etc?
 
 	}; // End of class AutoDiff
 
