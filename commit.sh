0ddb00f97c9734e4d0edef9c115fea69f87272e2
Tony Williams <tony.williams.1990@gmail.com>
2017-08-19 13:45:55 +0100

    automated_commit_test

---
+++ commit.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Script for commiting TSL to GitHub
+echo "Commit script"
+
+# Move unnecessary files
+mv ./figs ~/Desktop/figs_temp
+mv ./DATA ~/Desktop/DATA_temp
+mkdir ./DATA
+cp ~/Desktop/DATA_temp/.gitkeep ./DATA/.gitkeep
+
+# Setup commit message
+if [ $# -eq 0 ] ;
+then
+  message="empty"
+else
+  message=$1
+fi
+
+echo $message
+
+# GitHub stuff
+git add .
+git commit -m $message
+#git push origin master
+
+# Move the files back
+mv ~/Desktop/figs_temp ./figs
+rm -r DATA
+mv ~/Desktop/DATA_temp ./DATA
+
+exit 0
