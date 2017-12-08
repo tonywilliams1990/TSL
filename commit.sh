e182a06b0c13ec9d881bdfc13a04897b60fcd88f
Tony Williams <tony.williams.1990@gmail.com>
2017-12-08 17:00:20 +0000

    Merged_ODE_EVP

--- commit.sh
+++ commit.sh
@@ -25,9 +25,9 @@ echo $message
 # GitHub stuff
 git add .
 git commit -m $message
-#git push origin master
+git push origin master
 #git push origin Ridge
-git push origin ODE_EVP
+
 
 # Move the files back
 mv ~/Desktop/figs_temp ./figs
